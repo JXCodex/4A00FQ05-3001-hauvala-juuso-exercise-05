c9d063db3b337aecd056da5e77cea1bb3e4925e1
Juuso Hauvala <juuso.hauvala@tuni.fi>
2022-04-07 11:38:35 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const port = process.env.PORT || 8080;
 
 const db = [{ name: "tina" }, { name: "jack" }];
 
+let config = {
+  host: "mydb.tamk.fi",
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+};
+
+var connection = mysql.createConnection(config);
+app.get("/", (req, res) => {
+  connection.query("SELECT * FROM location", (error, results) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(results);
+    }
+  });
+});
+
 app.get("/names", (req, res) => {
   res.send(db);
 });
