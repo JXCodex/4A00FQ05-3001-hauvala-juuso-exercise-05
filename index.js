a261992d33fea36e10ab9def52130da59951ea39
Juuso Hauvala <juuso.hauvala@tuni.fi>
2022-04-07 11:53:44 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+const mysql = require("mysql");
 
 const db = [{ name: "tina" }, { name: "jack" }];
 
