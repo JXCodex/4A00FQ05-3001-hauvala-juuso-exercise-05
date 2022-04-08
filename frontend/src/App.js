b343e4bfc2fc39677e5f72bf89f75a82161dd737
Juuso Hauvala <juuso.hauvala@tuni.fi>
2022-04-08 10:26:12 +0300

    Added features of task 7

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,33 @@
 import "./App.css";
 import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+const axios = require("axios");
 
 class App extends React.Component {
   state = { locations: [] };
   async componentDidMount() {
-    let hr = await fetch("http://localhost:8080/locations");
-    let json = await hr.json();
-    this.setState({ locations: json });
+    axios.get("http://localhost:8080/locations").then((resp) => {
+      this.setState({ locations: resp.data });
+    });
   }
+
   render() {
-    if (this.state.locations.length === 0) {
-      return <p>loading...</p>;
-    } else {
-      let ui = this.state.locations.map((loc) => (
-        <li key={loc.id}>
-          {loc.id} - {loc.latitude} - {loc.longitude}
-        </li>
-      ));
-      return <ul>{ui}</ul>;
-    }
+    return (
+      <MapContainer center={[51.505, -0.09]} zoom={2} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {this.state.locations.map((loc) => (
+          <Marker position={[loc.latitude, loc.longitude]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    );
   }
 }
 export default App;
